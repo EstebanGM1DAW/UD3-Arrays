84e0f42f1dcc42ad765b260e3371d26255888673
EstebanGM1DAW <egonmorb@g.educaand.es>
2024-11-07 11:56:24 +0100

    Relacion7

--- src/Relacion7_2.java
+++ src/Relacion7_2.java
@@ -2,6 +2,6 @@ public class Relacion7_2 {
     // Crea un programa que reciba un 
     //Array de Strings y muestre el String más largo
     public static void main(String[] args) {
-        
+        System.out.println("Dame una frase: ");
     }
 }
