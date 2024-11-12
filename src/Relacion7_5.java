db7a27323a3a079edc8d473e9c5efa64e9c2ded5
EstebanGM1DAW <egonmorb@g.educaand.es>
2024-11-12 09:41:43 +0100

    relacion 7

---
+++ src/Relacion7_5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Relacion7_5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int tamaño = 10;
+        int[] numeros = new int[tamaño];
+        int sumaPos = 0;
+        int sumaNeg = 0;
+        int contPos = 0;
+        int contNeg = 0;
+        System.out.println("Introduce los elementos del array:");
+        for (int i = 0; i < tamaño; i++) {
+            System.out.println("Elemento " + (i + 1) + ": ");
+            numeros[i] = entrada.nextInt();   
+        }
+        for (int i = 0; i < tamaño; i++) {
+            if (numeros[i] > 0){
+                sumaPos = sumaPos+numeros[i];
+                contPos++;
+            } else if (numeros[i] < 0 ) {
+                sumaNeg = sumaNeg+numeros[i];
+                contNeg++;
+            }
+        }
+        System.out.println(sumaNeg/contNeg + " " + sumaPos/contPos);
+            entrada.close();
+    }
+}
