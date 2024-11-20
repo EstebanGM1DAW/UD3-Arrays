7f1bcb06405675e08c0a0346525eb35bbc889c94
EstebanGM1DAW <egonmorb@g.educaand.es>
2024-11-20 13:41:09 +0100

    total agregado

--- src/Relacion7_12.java
+++ src/Relacion7_12.java
@@ -2,6 +2,7 @@ public class Relacion7_12 {
     public static void main(String[] args) {
         int[][] arrayBid = new int[4][5];
         int[] sumaC = new int[5];
+        int total = 0;
         for (int fila = 0;fila<arrayBid.length;fila++) {
             for (int columna = 0; columna < arrayBid[fila].length;columna++) {
                 arrayBid[fila][columna] = (int) (Math.random()*100);
@@ -14,10 +15,13 @@ public class Relacion7_12 {
                 SumaFila += arrayBid[fila][columna];
                 sumaC[columna] += arrayBid[fila][columna];
             }
+            total += SumaFila;
             System.out.println("|" + SumaFila + "\t");
         }
         for (int i = 0; i < sumaC.length;i++) {
             System.out.print("|" + sumaC[i] + "\t");
+            total += sumaC[i];
         }
+        System.out.print("|"+total);
     }
 }
