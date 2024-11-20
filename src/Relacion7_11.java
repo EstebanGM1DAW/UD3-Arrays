e6ec890ab9c5a4836bd51e79281d50cb9c9bda50
EstebanGM1DAW <egonmorb@g.educaand.es>
2024-11-20 14:16:05 +0100

    cambiar los 0 por null para que no se muestren

--- src/Relacion7_11.java
+++ src/Relacion7_11.java
@@ -1,16 +1,20 @@
 public class Relacion7_11 {
     public static void main(String[] args) {
         // Definimos la matriz num
-        int num[][] = {
-            {0, 30, 2, 0, 0, 5},
-            {75, 0, 0, 0, 0, 0},
-            {0, 0, -2, 9, 0, 11}
+        Integer num[][] = {
+            {0, 30, 2, null, null, 5},
+            {75, null, null, null, 0, null},
+            {null, null, -2, 9, null, 11}
         };
     
             // Imprimimos la matriz
         for (int fila = 0; fila < num.length; fila++) {
             for (int columna = 0; columna < num[fila].length; columna++) {
+                if (num[fila][columna] != null) {
                 System.out.print(num[fila][columna] + "\t");
+                } else {
+                    System.out.print(" " + "\t");
+                }
             }
             System.out.println();
         }
