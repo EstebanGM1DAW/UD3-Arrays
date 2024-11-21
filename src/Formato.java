a2487710fb8b024c23c492d6814bb8c1bf56c136
EstebanGM1DAW <egonmorb@g.educaand.es>
2024-11-21 13:58:44 +0100

    Formato con printf

---
+++ src/Formato.java
@@ -0,0 +1,8 @@
+public class Formato {
+    public static void main(String[] args) {
+        int edad = 18;
+        float peso = 65.3f;
+        String nombre = "Jose";
+        System.out.printf("Hola, %s, tienes %d años y pesas %f kilos.", nombre, edad, peso);
+    }
+}
